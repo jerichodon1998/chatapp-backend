577cc67633b7c928768d944b5abcc14195182f15
markjericho <jerichodon1998@gmail.com>
2024-03-22 11:34:04 +0800

    setup dotenv

---
+++ env.d.ts
@@ -0,0 +1,6 @@
+namespace NodeJS {
+	interface ProcessEnv {
+		ENV: string;
+		PORT: string;
+	}
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from "express";
+import "dotenv/config";
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.ENV === "production" ? process.env.PORT : 3001;
 
 app.get("/", (req: Request, res: Response) => {
 	return res.status(200).json("Test");
