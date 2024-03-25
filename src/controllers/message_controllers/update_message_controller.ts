fddae11092b290d144d80cf81e21cb1886c51ac7
markjericho <jerichodon1998@gmail.com>
2024-03-25 22:14:59 +0800

    update message

---
+++ src/controllers/message_controllers/update_message_controller.ts
@@ -0,0 +1,24 @@
+import { RequestHandler } from "express";
+import Message from "../../models/Message";
+
+const updateMessageController: RequestHandler<
+	{ messageId: string },
+	{},
+	{ content: string }
+> = async (req, res) => {
+	const { messageId } = req.params;
+	const { content } = req.body;
+
+	try {
+		await Message.findByIdAndUpdate(messageId, {
+			$set: { content: content },
+		});
+
+		return res.status(200).json({ message: "Message updated!" });
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json({ message: "Something went wrong" });
+	}
+};
+
+export default updateMessageController;
--- src/routes/message_routes.ts
+++ src/routes/message_routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import sendMessageController from "../controllers/message_controllers/send_message_controller";
+import updateMessageController from "../controllers/message_controllers/update_message_controller";
 
 const router = Router();
 
 router.post("/", sendMessageController);
+router.put("/:messageId", updateMessageController);
 
 export default router;
