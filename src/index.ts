021e669d3ee98fb51bb5df2a879acf5dc393ecb6
markjericho <jerichodon1998@gmail.com>
2024-04-05 10:39:35 +0800

    modularize express (rest_api)

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,10 @@
-import express, { Request, Response } from "express";
 import "dotenv/config";
-import morgan from "morgan";
-import cookieParser from "cookie-parser";
-
 import DBConnection from "./database_configurations/DBConnection";
-import auth_routes from "./rest_api/routes/auth_routes";
-import channel_routes from "./rest_api/routes/channel_routes";
-import message_routes from "./rest_api/routes/message_routes";
-import { verifyToken } from "./rest_api/middlewares/token_middlewares/jwt_token_middleware";
+import app from "./rest_api";
 
-const app = express();
-const PORT = process.env.ENV === "production" ? process.env.PORT : 3001;
+// initialize connection to database
 DBConnection.dbConnect();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(morgan("dev"));
-
-app.get("/", async (req: Request, res: Response) => {
-	return res.json("Test");
-});
-
-app.get("/protected", verifyToken, async (req: Request, res: Response) => {
-	return res.json("Test protected route");
-});
-
-// authentication path
-app.use("/auth", auth_routes);
-// message path
-app.use("/message", message_routes);
-// channel path
-app.use("/channel", channel_routes);
+const PORT = process.env.ENV === "production" ? process.env.PORT : 3001;
 
 app.listen(PORT, () => console.log(`Server running on PORT:${PORT}`));
---
+++ src/rest_api/index.ts
@@ -0,0 +1,35 @@
+import express, { Request, Response } from "express";
+``;
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+
+import auth_routes from "../rest_api/routes/auth_routes";
+import channel_routes from "../rest_api/routes/channel_routes";
+import message_routes from "../rest_api/routes/message_routes";
+import { verifyToken } from "../rest_api/middlewares/token_middlewares/jwt_token_middleware";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(morgan("dev"));
+
+// test get route
+app.get("/", async (req: Request, res: Response) => {
+	return res.json("Test");
+});
+
+// test protected get route
+app.get("/protected", verifyToken, async (req: Request, res: Response) => {
+	return res.json("Test protected route");
+});
+
+// authentication path
+app.use("/auth", auth_routes);
+// message path
+app.use("/message", message_routes);
+// channel path
+app.use("/channel", channel_routes);
+
+export default app;
