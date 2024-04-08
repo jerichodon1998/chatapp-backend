0888c6bd087bb5141caa624dade3cc17592d621b
markjericho <jerichodon1998@gmail.com>
2024-04-08 19:17:24 +0800

    message namespace

---
+++ src/real_time/namespaces/messages/message_namespace.ts
@@ -0,0 +1,39 @@
+import { Namespace, Server } from "socket.io";
+import { messageHandler } from "../../handlers/message_handlers";
+
+// TODO - apply auth
+// TODO - validation/sanitization of user inputs
+class CustomNamespace {
+	#messageNamespace?: Namespace;
+
+	initializeNamespace(io: Server) {
+		// create a custom namespace
+		this.#messageNamespace = io.of("/messages");
+		// invoke onConn to test if working
+		this.#registerEvents();
+	}
+
+	#registerEvents() {
+		if (!this.#messageNamespace) {
+			throw new Error("Need to initialize namespace first");
+		}
+
+		// namespace test
+		this.#messageNamespace.on("connection", (socket) => {
+			console.log("namespace connected", socket.id);
+			socket.on("message", messageHandler);
+		});
+	}
+
+	getNamespace() {
+		if (!this.#messageNamespace) {
+			throw new Error("Need to initialize namespace first");
+		}
+
+		return this.#messageNamespace;
+	}
+}
+
+const messageNamespace = new CustomNamespace();
+
+export default messageNamespace;
