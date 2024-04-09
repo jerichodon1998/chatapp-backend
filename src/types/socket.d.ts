0b8c599c932ebcdfee969cee5ecedc601f205861
markjericho <jerichodon1998@gmail.com>
2024-04-09 16:11:01 +0800

    defined types modified

--- src/types/socket.d.ts
+++ src/types/socket.d.ts
@@ -1,3 +1,7 @@
-type CallbackResponse = (response: { status: number; message: string }) => void;
+type ICallbackResponse = (response: {
+	status: number;
+	message: string;
+}) => void;
 
 interface ISendMessageSocketPayload extends ISendMessage {}
+interface IDeleteMessageSocketPayload extends IDeleteMessageReqParam {}
