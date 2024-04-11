560c43184872cfcf3af7f06d536cc9ffc1640eea
markjericho <jerichodon1998@gmail.com>
2024-04-11 11:26:04 +0800

    reconfig socketio server

--- src/real_time/index.ts
+++ src/real_time/index.ts
@@ -1,11 +1,32 @@
 import { Server } from "socket.io";
 import { Server as httpServer } from "http";
 import messageNamespace from "./namespaces/messages/message_namespace";
+import { serialize } from "cookie";
 class SocketServer {
 	#io?: Server;
 
 	initializeServer(server: httpServer) {
-		this.#io = new Server(server);
+		this.#io = new Server(server, {
+			cors: {
+				origin: "http://localhost:3000",
+				credentials: true,
+			},
+		});
+
+		// TODO - rewrite/fix when starting/while implementing auth with frontend
+		// NOTE - The token will be used should be the one from signin/signup token
+		// 			 from the express server http cookie
+		this.#io.engine.on("initial_headers", (headers, request) => {
+			console.log("Handshake done");
+			headers["set-cookie"] = serialize("Bearer", "myAccessToken", {
+				sameSite: "none",
+				httpOnly: true,
+				secure: true,
+				path: "/",
+				maxAge: 60 * 60,
+			});
+		});
+
 		// invoke initializeNamespaces
 		this.initializeNamespaces();
 	}
