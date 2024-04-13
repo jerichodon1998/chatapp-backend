929a1393de5288c14375af88585ac11ee3299d7e
markjericho <jerichodon1998@gmail.com>
2024-04-13 09:12:36 +0800

    remove cookie package and remove io initial_headers implementations

--- src/real_time/index.ts
+++ src/real_time/index.ts
@@ -1,7 +1,6 @@
 import { Server } from "socket.io";
 import { Server as httpServer } from "http";
 import messageNamespace from "./namespaces/messages/message_namespace";
-import { serialize } from "cookie";
 class SocketServer {
 	#io?: Server;
 
@@ -11,20 +10,6 @@ class SocketServer {
 				origin: "http://localhost:3000",
 				credentials: true,
 			},
-		});
-
-		// TODO - rewrite/fix when starting/while implementing auth with frontend
-		// NOTE - The token will be used should be the one from signin/signup token
-		// 			 from the express server http cookie
-		this.#io.engine.on("initial_headers", (headers, request) => {
-			console.log("Handshake done");
-			headers["set-cookie"] = serialize("Bearer", "myAccessToken", {
-				sameSite: "none",
-				httpOnly: true,
-				secure: true,
-				path: "/",
-				maxAge: 60 * 60,
-			});
 		});
 
 		// invoke initializeNamespaces
