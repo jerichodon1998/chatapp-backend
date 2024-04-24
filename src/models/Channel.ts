168d5e0f1fcd1ede12114423bdadcf6bf25cd660
markjericho <jerichodon1998@gmail.com>
2024-04-24 21:51:20 +0800

    channels schema and interface modified (add field: directChannelMergedIds)

--- src/models/Channel.ts
+++ src/models/Channel.ts
@@ -9,6 +9,7 @@ const channelSchema = new Schema<IChannel>({
 	admins: { type: [Schema.Types.ObjectId], ref: "User" },
 	members: { type: [Schema.Types.ObjectId], required: true, ref: "User" },
 	channelType: { type: String, required: true },
+	directChannelMergedIds: { type: String, unique: true },
 });
 
 const Channel = model<IChannel>("Channel", channelSchema);
--- src/types/channel.d.ts
+++ src/types/channel.d.ts
@@ -1,9 +1,10 @@
 type ChannelTypes = "direct" | "group";
 
 interface IChannel {
-	admins?: [object];
+	admins?: [object]; // admin will be populated if and only if channelType is 'group'
 	members: [object];
 	channelType: ChannelTypes;
+	directChannelMergedIds?: string; // merged two Ids into a string with '-' e.g. ("userId1-userId2") if and only if channelType = "direct"
 }
 
 interface IFetchChannelReqParam {
