4f5a5e167d547bf020544fd322164dc3ba90e2fd
markjericho <jerichodon1998@gmail.com>
2024-04-24 16:42:02 +0800

    create interfaces/types for custom namespaces

--- src/types/socket.d.ts
+++ src/types/socket.d.ts
@@ -1,16 +1,75 @@
 import { Namespace, Socket } from "socket.io";
 import { Server as httpServer } from "http";
+import { mongo } from "mongoose";
 
 export type ICallbackResponse = (response: {
 	status: number;
 	message: string;
 }) => void;
 
-export type NamespaceNames = "channels" | "messages";
+export type TNamespaceNames = "channels" | "messages";
+
+export interface IMessageClientToServerEvents {
+	messageSend: (
+		payload: ISendMessageSocketPayload,
+		callback: ICallbackResponse
+	) => Promise<void>;
+	messageUpdate: (
+		payload: IEditMessageSocketPayload,
+		callback: ICallbackResponse
+	) => Promise<void>;
+	messageDelete: (
+		payload: IDeleteMessageSocketPayload,
+		callback: ICallbackResponse
+	) => Promise<void>;
+}
+
+export interface IMessageServerToClientEvents {
+	messageSend: (
+		data: mongo.ChangeStreamInsertDocument<mongo.BSON.Document>,
+		callback: ICallbackResponse
+	) => void;
+	messageUpdate: (
+		data: mongo.ChangeStreamUpdateDocument,
+		callback: ICallbackResponse
+	) => void;
+	messageDelete: (
+		data: mongo.ChangeStreamDeleteDocument,
+		callback: ICallbackResponse
+	) => void;
+}
+
+export interface IChannelServerToClientEvents {
+	channelCreate: (
+		data: mongo.ChangeStreamInsertDocument<mongo.BSON.Document>,
+		callback: ICallbackResponse
+	) => void;
+	channelUpdate: (
+		data: mongo.ChangeStreamUpdateDocument,
+		callback: ICallbackResponse
+	) => void;
+	channelDelete: (
+		data: mongo.ChangeStreamDeleteDocument,
+		callback: ICallbackResponse
+	) => void;
+}
+
+export type TChannelNamespace = Namespace<{}, IChannelServerToClientEvents>;
+
+export type TMessageNamespace = Namespace<
+	IMessageClientToServerEvents,
+	IMessageServerToClientEvents
+>;
+
+export type TypeOfNamespace<T> = T extends "messages"
+	? TMessageNamespace
+	: T extends "channels"
+	? TChannelNamespace
+	: never;
 
 export interface ICustomNamespace {
 	name: NamespaceNames;
-	namespace: Namespace | undefined;
+	namespace: TypeOfNamespace | undefined;
 	socket: Socket | undefined;
 
 	initializeNamespace: (io: Server) => void;
