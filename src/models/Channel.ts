201783266efff345f435e4ff58632c063458dda3
markjericho <jerichodon1998@gmail.com>
2024-04-25 10:12:31 +0800

    remove comment (solved concerns)

--- src/models/Channel.ts
+++ src/models/Channel.ts
@@ -1,10 +1,5 @@
 import { Schema, model } from "mongoose";
 
-// NOTE - mongodb indexes doesn't have a unique permutation on array elements.
-// TODO - add a field where the two IDs of the members from a direct channelType
-// 			be concatenated with '-' e.g. userId1-userId2 and create an index from it.
-// NOTE - See also the TODO from the User's Model. I think either of one is a good solution.
-// NOTE - This is related to 'improve join rooms' from message_namespace TODO
 const channelSchema = new Schema<IChannel>({
 	admins: { type: [Schema.Types.ObjectId], ref: "User" },
 	members: { type: [Schema.Types.ObjectId], required: true, ref: "User" },
