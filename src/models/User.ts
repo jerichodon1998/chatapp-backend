85804975c945ddf825f6dd793eaec78332e350db
markjericho <jerichodon1998@gmail.com>
2024-04-25 10:03:33 +0800

    add 'channels' field to user schema

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,5 @@
 import { Schema, model } from "mongoose";
+import { IUser } from "../types/user";
 
 // TODO - add field that holds all the channelId(s) that the user is involved in.
 // NOTE - See also the TODO from the Channel's Model. I think either of one is a good solution.
@@ -7,6 +8,7 @@ const userSchema = new Schema<IUser>({
 	email: { type: String, required: true, unique: true },
 	password: { type: String, required: true, unique: true },
 	username: { type: String, required: true },
+	channels: { type: [Schema.Types.ObjectId] },
 });
 
 const User = model<IUser>("User", userSchema);
