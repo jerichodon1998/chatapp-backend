b43df6d5b15aba9c0d910adc2739d25cf5f50b81
markjericho <jerichodon1998@gmail.com>
2024-05-20 14:43:21 +0800

    auth smoke test

---
+++ src/tests/smoke/auth.smoke.test.ts
@@ -0,0 +1,60 @@
+import { afterAll, describe, test } from "@jest/globals";
+import DBConnection from "../../../src/database_configurations/DBConnection";
+import restAPI from "../../../src/rest_api";
+import { Server } from "http";
+import { ISignin } from "UserTypes";
+import request from "supertest";
+
+describe("Authentication tests:", () => {
+	const PORT = 3001;
+	const signinEndPoint = "/auth/signin";
+	const userCredentials: ISignin = {
+		email: "mark@email.com",
+		password: "password",
+	};
+	let api: null | Server = null;
+
+	beforeAll(async () => {
+		try {
+			await DBConnection.dbConnect();
+			api = restAPI.listen(PORT);
+		} catch (error) {
+			console.error(
+				"Failed to connect to the database or initialize API:",
+				error
+			);
+			throw error;
+		}
+	});
+
+	// TODO - Implement this after creating a delete user route and controller
+	test.todo("Should signup successfully with right input credentials");
+
+	test("Should login successfully with right credentials", async () => {
+		if (!api) {
+			throw new Error("API is null");
+		}
+
+		const response = await request(api)
+			.post(signinEndPoint)
+			.send(userCredentials);
+
+		const tokenKeyValuePair = response.headers["set-cookie"][0]
+			.split(";")[0]
+			.split("=");
+		const tokenKey = tokenKeyValuePair[0];
+		const tokenValue = tokenKeyValuePair[1];
+
+		expect(response.statusCode).toBe(200);
+		expect(tokenKey).toBe("Bearer");
+		expect(tokenValue).not.toBeFalsy();
+	});
+
+	// TODO - Implement this after creating a delete user route and controller
+	test.todo("Should delete user account");
+
+	afterAll(async () => {
+		await DBConnection.dbDisconnect();
+		api?.close();
+	});
+});
