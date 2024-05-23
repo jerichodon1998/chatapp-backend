f264c112c2c22d5d8c241af001a99e814746d9b2
markjericho <jerichodon1998@gmail.com>
2024-05-23 11:49:39 +0800

    logout feature

--- src/rest_api/routes/auth_routes.ts
+++ src/rest_api/routes/auth_routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import signinController from "../controllers/auth_controllers/signin_controller";
 import signupController from "../controllers/auth_controllers/signup_controller";
+import logoutController from "../controllers/auth_controllers/logout_controller";
 
 const router = Router();
 
 router.post("/signin", signinController);
 router.post("/signup", signupController);
+router.post("/logout", logoutController);
 
 export default router;
